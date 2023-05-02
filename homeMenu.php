9272a59e2d162aa251a0d4836810a0add3eb0825
RAJAT SINGH RAWAT <53485618+rajatrawatcensored@users.noreply.github.com>
2023-05-02 23:43:24 +0530

    home menu page added

---
+++ homeMenu.php
@@ -0,0 +1,29 @@
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style1.css"> 
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid" >
+    <div class="navbar-header" style="color:red">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+       <ul class="nav navbar-nav navbar-right">
+        <li><a href="#myPage">HOME</a></li>
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#user">STUDENT</a></li>
+        <li><a href="#admin">ADMIN</a></li>
+		&nbsp;&nbsp;&nbsp;&nbsp;
+      </ul>
+      </ul>
+    </div>
+  </div>
+</nav>
